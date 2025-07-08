17b7b981b423aae27990a5e286c1cc482e98d041
Michael Obubelebra Amachree <amachree9630@gmail.com>
2025-07-08 13:25:33 +0100

    Remove commented-out theme toggle button code

--- src/lib/components/shared/Theme.svelte
+++ src/lib/components/shared/Theme.svelte
@@ -11,25 +11,8 @@
 
     const switchTheme = (theme: "light" | "dark" | "system") => {
         selectedTheme.current = theme;
-        // setMode(theme);
     };
 </script>
-
-<!-- <Button onclick={toggleMode} variant="outline" size="icon">
-    {#if selectedTheme.current === "system"}
-        <Computer
-            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all"
-        />
-    {:else}
-        <SunIcon
-            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
-        />
-        <MoonIcon
-            class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100"
-        />
-    {/if}
-    <span class="sr-only">Toggle theme</span>
-</Button> -->
 
 <DropdownMenu.Root>
     <DropdownMenu.Trigger
